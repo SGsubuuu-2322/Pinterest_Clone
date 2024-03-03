5c9c9005e0c296ef1d5dc3a851dc259cb0b8ed88
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-03 21:09:29 +0530

    Imported passport related libraries and configured it up...

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/pinterest_clone");
 
@@ -31,5 +32,7 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("User", userSchema);
