277ffd3bb2c8c5298887a9c686218e6c6a97b356
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-03 20:55:10 +0530

    Added some well defined routes with its controller action...

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,23 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/createuser", async function (req, res) {
+  const createdUser = await userModel.create({
+    userName: "SGsubu@2322",
+    password: "SGshreyans@232214",
+    posts: [],
+    email: "SGsubuuu143@gmail.com",
+    fullName: "Subham Kumar Pradhan",
+  });
+
+  res.send(createdUser);
+});
+router.get("/createpost", async function (req, res) {
+  const createdPost = await postModel.create({
+    postText: "Hii Bhaiyaa",
+  });
+
+  res.send(createdPost);
+});
+
 module.exports = router;
