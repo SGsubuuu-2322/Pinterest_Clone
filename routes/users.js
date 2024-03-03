0cb382514e723f38793a8ecf1ba0f64309dea33b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-03 19:39:34 +0530

    Created a user model and created a db for our project and connected it to our app...

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest_clone");
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  dp: {
+    type: String, // Assuming dp is a URL or file path
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = router;
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
